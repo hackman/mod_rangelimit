80881104a0025a55231d7e5ca92162ae19c6b24f
Marian Marinov <mm@yuhu.biz>
2011-09-27 10:37:14 +0300

    mod_rangelimit.h: Added support for Apache 1.3

--- mod_rangelimit.h
+++ mod_rangelimit.h
@@ -1,14 +1,22 @@
+#ifndef APACHE_RELEASE
+#define APACHE2
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
 #include "httpd.h"
 #include "http_config.h"
 #include "http_core.h"
 #include "http_log.h"
 #include "http_main.h"
 #include "http_protocol.h"
-#include "http_request.h"
 #include "util_script.h"
-#include "http_connection.h"
 
-#include "apr_strings.h"
-
-#include <stdio.h>
+#ifdef APACHE2
 #include <strings.h>
+
+#include "http_request.h"
+#include "http_connection.h"
+#include "apr_strings.h"
+#endif
